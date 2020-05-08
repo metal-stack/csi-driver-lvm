fea5b08de47442a04f2a66563145361b5fab4064
Markus Wennrich <markus.wennrich@f-i-ts.de>
2020-05-08 11:09:24 +0200

    update builder to golang:1.14-alpine

--- start-minikube-on-linux.sh
+++ start-minikube-on-linux.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-minikube start --memory 5g
+minikube start --memory 5g --driver kvm2
 minikube ssh 'for i in 0 1; do fallocate -l 1G loop${i} ; sudo losetup -f loop${i}; sudo losetup -a ; done'
 minikube ssh 'sudo rm /sbin/losetup'
 scp -o 'StrictHostKeyChecking=no' -i $(minikube ssh-key) $(which losetup)  docker@$(minikube ip):/tmp/losetup
