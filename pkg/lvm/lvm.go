8700605e004e85456ef1cd6da1b35dea733c41c3
Markus Wennrich <markus.wennrich@f-i-ts.de>
2021-05-12 08:36:04 +0200

    remove maxStorageCapacity restriction (#53)

--- pkg/lvm/lvm.go
+++ pkg/lvm/lvm.go
@@ -38,13 +38,6 @@ import (
 	"k8s.io/klog/v2"
 )
 
-const (
-	kib int64 = 1024
-	mib int64 = kib * 1024
-	gib int64 = mib * 1024
-	tib int64 = gib * 1024
-)
-
 // Lvm contains the main parameters
 type Lvm struct {
 	name              string
